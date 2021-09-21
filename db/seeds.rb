3d4f81b3617f08fac27f1210a212d870a93ba4a9
Kaia <kbergmanndean@gmail.com>
2021-09-21 14:39:41 -0400

    wrote half artworks in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Artwork.create(name:"Evening at Kuerners", artist:"Andrew Wyeth", url:"https://s31531.pcdn.co/wp-content/uploads/2017/07/EveningAtKuerners_1970_AndrewWyeth_ArtistsNetwork.png", year:1970)
+Artwork.create(name:"The Hunters in the Snow", artist:"Pieter Bruegel the Elder", url:"https://media.izi.travel/712b0128-72a7-49c8-ae47-06c2adccc4d6/53fd3e2f-6542-4d43-9caf-905b1678eaef_800x600.jpg", year:1565)
+Artwork.create(name:"Monticello", artist:"Jean-Michel Basquiat", url:"https://uploads2.wikiart.org/images/jean-michel-basquiat/monticello.jpg!Large.jpg", year:1983)
