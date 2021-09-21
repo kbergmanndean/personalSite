0f96a7178c0b689821454b6b9ede1f33635b03b1
Kaia <kbergmanndean@gmail.com>
2021-09-21 15:13:57 -0400

    added index and show to artworks controller

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,2 +1,15 @@
 class ArtworksController < ApplicationController
+
+    def index
+        artworks=Artwork.all
+        render json: artworks
+    end
+
+    def show
+        artwork=Artwork.find_by(id:params[:id])
+        render json: artwork
+    end
+
+
+
 end
