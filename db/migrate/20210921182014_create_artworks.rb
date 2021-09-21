5f1aa01e9fcf5239ffefe561b2e8286d3b0626a8
Kaia <kbergmanndean@gmail.com>
2021-09-21 14:29:39 -0400

    made columns for migration

--- db/migrate/20210921182014_create_artworks.rb
+++ db/migrate/20210921182014_create_artworks.rb
@@ -1,6 +1,10 @@
 class CreateArtworks < ActiveRecord::Migration[6.1]
   def change
     create_table :artworks do |t|
+      t.string :name
+      t.string :url 
+      t.string :artist
+      t.integer :year
 
       t.timestamps
     end
