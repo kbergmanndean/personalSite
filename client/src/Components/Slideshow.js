0ae59f22d8222f9bd661efaf4721b9b6013d4e8e
Kaia <kbergmanndean@gmail.com>
2021-09-25 13:45:56 -0400

    add bootstrap carousel

--- client/src/Components/Slideshow.js
+++ client/src/Components/Slideshow.js
@@ -10,28 +10,34 @@ function Slideshow(){
             const res= await fetch (`http://localhost:3000/artworks`);
             const artworkData=await res.json();
             setArtworks(artworkData);
-            setWork(artworkData[0]);
-            let i=0
-            i<artworkData.length-1?
-                i++:i=0;
-            setTimeout(()=>{setWork(artworkData[i])},2500);
-            console.log(i)
-            console.log(work)
+            // setWork(artworkData[0]);
+            // let i=0
+            // i<artworkData.length-1?
+            //     i++:i=0;
+            // setTimeout(()=>{setWork(artworkData[i])},2500);
+            // console.log(i)
+            // console.log(work)
             console.log(artworks)
         }
         fetchArt();
     },[]);
 
-    
-    
-
-
-
-
-
     return(
         <div>
-            <img src={work.url}/>
+            <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img className="d-block w-100" src={artworks[0].url}/>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={artworks[1].url}/>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={artworks[2].url}/>
+                    </div>
+                </div>
+            </div>
+            {/* <img src={work.url}/> */}
             {/* {artworks.map((artwork)=><img src={artwork.url} key={artwork.id}/>)}   */}
         </div>
     )
