f34a9010aa50d238acdc5187bd92d939527aeaed
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-10-04 15:28:54 -0400

    Revert "Centered resume pdf"
    This reverts commit 4beca0e054ea906d836ecd5b147ec0ebed5452b4.

--- client/src/Components/Resume.js
+++ client/src/Components/Resume.js
@@ -3,13 +3,11 @@ import resume from "./resume.pdf"
 
 function Resume(){
     return(
-        <div className="resume-page">
-            <div className="resume-item">
+        <div>
             <h1>Resume</h1>
             <Document file={resume}>
                 <Page pageNumber={1}/>
             </Document>
-            </div>
         </div>
 
     )
