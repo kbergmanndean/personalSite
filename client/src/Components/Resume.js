a449ed2a7fecddec9ac95d2824fe9b49d81d0584
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-10-06 14:43:20 -0400

    got carousel centered and smaller

--- client/src/Components/Resume.js
+++ client/src/Components/Resume.js
@@ -2,7 +2,12 @@ function Resume(){
     return(
         <div>
             <h1>Resume</h1>
-            <iframe src="../resume.pdf" width="50%" height="500pt" title="Resume">Resume</iframe>
+            {/* <iframe src="../resume.pdf" width="50%" height="500pt" title="Resume">Resume</iframe> */}
+            <iframe
+                title="file"
+                style={{ width: '100%', height: '100%' }}
+                src="../resume.pdf"
+            />
         </div>
 
     )
