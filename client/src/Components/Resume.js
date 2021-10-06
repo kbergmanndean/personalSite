de6d934a6a6c24d2807aed9a20bd75b02cf11018
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-10-06 16:11:10 -0400

    styled

--- client/src/Components/Resume.js
+++ client/src/Components/Resume.js
@@ -3,6 +3,8 @@ import pdf from "../resume.pdf"
 function Resume(){
     return(
         <div id="resume-page">
+            <br/>
+            <a href="/">Home</a>
             <h1>Resume</h1>
             <iframe
                 id="resume"
