d40328130ab7ab7b2dde56855a948ff0914f403e
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-10-08 13:16:53 -0400

    slideshow works again

--- client/src/Components/Slideshow.js
+++ client/src/Components/Slideshow.js
@@ -53,6 +53,14 @@ function Slideshow(){
                         </div>
                     </div>
                 </div>
+                {/* <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a> */}
             </div>
             :<h4>Content Loading</h4>}
         </div>
