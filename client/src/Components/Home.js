190d6995df194a3eb324048e1d2bdb69d3f5c8b5
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-10-08 13:57:59 -0400

    linkedin angel

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -9,13 +9,15 @@ function Home(){
         <div id="home-page">
             <br/>
         <h1>Kaia Bergmann-Dean</h1>
-        <ul className="links">
-            <li><a href="https://www.linkedin.com/in/kaiabergmann-dean/">LinkedIn</a></li>
-            <li><a href="https://github.com/kbergmanndean">GitHub</a></li>
-            <li><a href="/resume">Resume</a></li>
-            <li>Projects</li>
-        </ul>
+    
+        <a href="https://github.com/kbergmanndean">GitHub</a>
+        <a href="/resume">Resume</a>
+        <span>Projects</span>
+        <div id="linkedin-div">
         <img className="cherub" id="cherub1" alt="cherub" src={cherub}/>
+        <br/>
+        <a href="https://www.linkedin.com/in/kaiabergmann-dean/">LinkedIn</a>
+        </div>
         <Slideshow/>
         </div>
     )
