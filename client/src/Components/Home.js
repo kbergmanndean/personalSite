c1613b82e45fca79fee9dee5769cdd1293279ed0
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-10-08 14:09:21 -0400

    added github raccoon

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,5 +1,6 @@
 import Slideshow from "./Slideshow"
 import cherub from "../pngs/cherub1.png"
+import raccoon from "../pngs/raccoon.png"
 
 function Home(){
     
@@ -10,13 +11,18 @@ function Home(){
             <br/>
         <h1>Kaia Bergmann-Dean</h1>
     
-        <a href="https://github.com/kbergmanndean">GitHub</a>
+        
         <a href="/resume">Resume</a>
         <span>Projects</span>
         <div id="linkedin-div">
-        <img className="cherub" id="cherub1" alt="cherub" src={cherub}/>
-        <br/>
-        <a href="https://www.linkedin.com/in/kaiabergmann-dean/">LinkedIn</a>
+            <img id="cherub1" alt="cherub" src={cherub}/>
+            <br/>
+            <a href="https://www.linkedin.com/in/kaiabergmann-dean/">LinkedIn</a>
+        </div>
+        <div id="github-div">
+            <img id="raccoon" alt="raccoon" src={raccoon}/>
+            <br/>
+            <a href="https://github.com/kbergmanndean">GitHub</a>
         </div>
         <Slideshow/>
         </div>
