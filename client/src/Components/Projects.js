f3894f93c997e01f6699924d5832397a7d2daae1
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-10-12 17:26:13 -0400

    Added embedded youtube video

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -1,6 +1,10 @@
 function Projects(){
     return(
+        <div>
         <h1>Projects</h1>
+        <iframe src="https://www.youtube.com/watch?v=yDaJDPpVqiw" width="300pt"/>
+        </div>
+
     )
 }
 
