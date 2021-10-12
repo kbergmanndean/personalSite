b7e9357a604ed68e96405b3a246efd08ed396736
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-10-12 18:23:34 -0400

    commented plans to fetch projects and render

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -1,4 +1,15 @@
+
+
 function Projects(){
+
+    //create state
+    //function to fetch projects,
+    //put projects in state
+    //display projects in iframes below
+
+
+
+
     return(
         <div>
         <h1>Projects</h1>
