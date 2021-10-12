4867380d2ece11b535fc9fe55a7372f8398c2971
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-10-12 17:56:53 -0400

    added projects model and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :artworks, only: [:index, :show]
-
+  resources :projects, only: [:index, :show]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
