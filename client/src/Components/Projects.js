86626c98eb2232dba670951c84252c3661e40149
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-10-13 17:51:26 -0400

    formatting for medready vid ok

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -12,16 +12,28 @@ function Projects(){
             console.log(projectData)
         }
         fetchProjects();
-    },[])
+    },[]);
     //display projects in iframes below
-
-
-
 
     return(
         <div>
-        <h1>Projects</h1>
-        <iframe src="https://www.youtube.com/embed/yDaJDPpVqiw"/>
+            <br/>
+            <h1>Projects</h1>
+            <br/>
+            {/* <ul>
+            {projects.length>0?projects.map((project)=>{<li><iframe src={projects[0].url}/></li>}):<h3>Content Loading</h3>}
+            </ul> */}
+            {projects.length>0?
+            <div>
+                <div id="medready-div">
+                    <h2>{projects[0].name}</h2>
+                    <iframe className="project" src={projects[0].url}/>
+                </div>
+                <div id="museum-div">
+                    <h2>{projects[1].name}</h2>
+                </div>
+            </div>
+            :<h3>Content Loading</h3>}       
         </div>
 
     )
