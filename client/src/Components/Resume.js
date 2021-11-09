4c300df0521f867727c80079110a8b8f91513354
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-11-09 17:40:01 -0500

    added skunk to home link on resume page

--- client/src/Components/Resume.js
+++ client/src/Components/Resume.js
@@ -1,11 +1,13 @@
 import pdf from "../resume.pdf"
 import home from "../pngs/home.png"
+import skunk from "../pngs/skunk.png"
 
 function Resume(){
     return(
         <div>
             <br/>
             <a href="/">
+                <img id="skunk" src={skunk}/>
                 <img className="home-link" src={home}/>
             </a>
             <br/>
